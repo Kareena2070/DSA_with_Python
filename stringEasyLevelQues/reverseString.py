17d79f69a9e2ab4d70e73f8c9251f38b8020fa5f
Kareena2070 <kareenayadav1088@gmail.com>
2025-08-07 10:39:32 +0530

    solve reverse a string with built-in and manual method

---
+++ stringEasyLevelQues/reverseString.py
@@ -0,0 +1,38 @@
+#  Reverse a String
+
+name = "navgurukul is my best place"
+def reverseString1(string):
+    revserString = string[::-1]
+    print(revserString)
+
+reverseString1(name)
+
+# ✅ Time Complexity
+# Python slicing string[::-1] internally creates a new string, copying characters in reverse order.
+# It must go through all n characters once →
+# 🕒 Time = O(n)
+# ✅ Space Complexity
+# Creates a new reversed string → occupies n space.
+# Original string is not modified.
+# 🧠 Space = O(n)
+
+
+def reverseString2(string):
+    n = len(string)
+    newString = ""
+    for i in range(n-1, -1, -1):
+        newString+=string[i]
+    print(newString)
+reverseString2(name)
+
+# ✅ Time Complexity
+# Loop runs n times → that part is O(n)
+# But newString += string[i] creates a new copy every time (since strings are immutable in Python) → O(n) for each += step
+# So the full loop becomes:O(1 + 2 + 3 + ... + n) = O(n²)
+# 🕒 Time = O(n²) ← slower
+# ✅ Space Complexity
+# One final string of length n is returned.
+# Intermediate temporary strings are garbage collected.
+# 🧠 Space = O(n)
+
+
