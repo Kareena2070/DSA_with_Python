ead1647f1e5bd7028c19e1cd3d7528c04be994b9
Kareena2070 <kareenayadav1088@gmail.com>
2025-08-08 11:35:46 +0530

    solve find first non-repeating character

---
+++ stringEasyLevelQues/non-repeating.py
@@ -0,0 +1,24 @@
+# Find First Non-Repeating Character
+
+word = "aabbccde"
+
+def nonRepeating(word):
+    for i in range(len(word)):
+        count = 0
+        char = word[i]
+        for j in range(len(word)):
+            if char == word[j]:
+                count+=1
+        if count == 1:
+            print(char)
+            break
+
+nonRepeating(word)
+
+# time complexity 
+# outer loop runs O(N) time and also inner loop runs O(N) times 
+# so total time complexity = O(n^2)
+# space complexity is = O(n), no such extra space added
+
+     
+        
