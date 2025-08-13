fb8d5a0ac32467c1db240e9fc30ae371b3aa1321
Kareena2070 <kareenayadav1088@gmail.com>
2025-08-13 10:46:39 +0530

    use better, pythonic and otimal approach to solve remove duplicate from strings

--- stringEasyLevelQues/removeDuplicate.py
+++ stringEasyLevelQues/removeDuplicate.py
@@ -2,6 +2,9 @@
 
 word = "bcabc123!@#"
 
+
+# brute force approach
+
 def removeDuplicate(word):
     duplicate = ""
     for i in word:
@@ -15,4 +18,44 @@ removeDuplicate(word)
 # inter loop runs O(n) => check each char in duplicate
 # total time complexity === O(n)
 # Space complexity === O(n) 
+
+
+
+# better approch
+def remove(word):
+    string = set(word)
+    print("".join(string))
+
+remove(word)
+# time complexity === O(n)
+# space complexity === O(n) because join (joining the set back into a string )
+# ⚠ But order of characters is not guaranteed.
+
+
+# optimal approcah 
+def remove(word):
+    seen = set()
+    result = []
+    for ch in word:
+        if ch not in seen:
+            seen.add(ch)
+            result.append(ch)
+    print("".join(result))
+
+word = "banana"
+remove(word)
+# set ==  O(1) space allocation
+# outer loop == O(n) iterations
+#  if condition == O(1) average
+# add === O(1) average
+# space complexity ===O(n)
+
+
+
+# Pythonic One-Liner
+word = "banana"
+print("".join(dict.fromkeys(word)))  # Output: ban
+
+# Final Time Complexity: O(n)
+#  Space Complexity: O(n) (dictionary + output string)
 
