5105cd1d6eafb9b7d20d32407730d5d54dd16346
Kareena2070 <kareenayadav1088@gmail.com>
2025-09-21 16:47:58 +0530

    implement in-place O(n) solution with two-pointe

--- leetCodeQuestion/esayLevel/move-zero.py
+++ leetCodeQuestion/esayLevel/move-zero.py
@@ -1,5 +1,4 @@
 # 283. Move Zeroes
-
 # Given an integer array nums, move all 0's to the end of it while maintaining the relative order of the non-zero elements.
 # Note that you must do this in-place without making a copy of the array.
 
@@ -37,4 +36,17 @@ print(num(nums))
 
 # time complexity = O(n^2)
 # space complexity = O(1)
+
+# best approach in which all test case pass on leetcode 
+def moveZeroes( nums):
+    j = 0
+    for i in range(len(nums)):
+        if nums[i] !=0:
+            nums[i], nums[j] = nums[j], nums[i]
+            j+=1
+    return nums
+
+nums = [0,1,0,3,12]
+print(moveZeroes(nums))
+        
 
