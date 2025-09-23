1feb375b235f2c81e586676ab0bb3977df379ec5
Kareena2070 <kareenayadav1088@gmail.com>
2025-09-23 10:59:37 +0530

    Update merge to optimal LeetCode solution with in-place modification

--- leetCodeQuestion/esayLevel/mergeSortedArray.py
+++ leetCodeQuestion/esayLevel/mergeSortedArray.py
@@ -57,7 +57,7 @@ n = 3
 # nums2 = [1]
 # n = 1
 
-print(merge(nums1, m, nums2, n)) #[1, 2, 2, 3, 5, 6]
+# print(merge(nums1, m, nums2, n)) #[1, 2, 2, 3, 5, 6]
 
 
 
@@ -80,4 +80,28 @@ m = 1
 nums2 = []
 n = 0
 
-print(merge(nums1, m, nums2, n)) #[1]    
+# print(merge(nums1, m, nums2, n)) #[1]    
+
+
+# Optimal Approch (no extra space)
+def merge(nums1, m, nums2, n):
+    i = m - 1
+    j = n - 1
+    k = m + n - 1
+    
+    while j >= 0:
+        if i >= 0 and nums1[i] > nums2[j]:
+            nums1[k] = nums1[i]
+            i -= 1
+        else:
+            nums1[k] = nums2[j]
+            j -= 1
+        k -= 1
+    return nums1    #this return here for local uses if thsi code on leetcode then this return does needed
+
+nums1 = [1,2,3,0,0,0]
+m = 3
+nums2 = [2,5,6]
+n = 3
+
+print(merge(nums1, m, nums2, n)) #[1, 2, 2, 3, 5, 6]
