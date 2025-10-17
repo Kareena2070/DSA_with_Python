4bd301cde767eac749ca80feb2a259f3114bfe27
Kareena2070 <kareenayadav1088@gmail.com>
2025-10-17 14:57:36 +0530

    solve leetcode question 33. Search in Rotated Sorted Array--binary search approach

--- leetCodeQuestion/mediumLevel/searchInRotatedSortedArray.py
+++ leetCodeQuestion/mediumLevel/searchInRotatedSortedArray.py
@@ -21,6 +21,7 @@
 # Input: nums = [1], target = 0
 # Output: -1
 
+# linear search approach
 nums = [4,5,6,7,0,1,2]
 target = 0
 
@@ -31,4 +32,29 @@ def searchInRotatedSortedArray(nums, target):
     return -1
 print(searchInRotatedSortedArray(nums, target))
 # Time complexity = O(n)
+# Space complexity = O(1)
+
+# binary search approach
+def searchInRotatedSortedArray2(nums, target):
+    left, right = 0, len(nums)-1
+
+    while left <= right:
+        mid = (left + right)//2
+
+        if nums[mid] == target:
+            return mid
+
+        if nums[left] <= nums[mid]:
+            if nums[left] <= target < nums[mid]:
+                right = mid - 1
+            else:
+                left = mid + 1
+        else:
+            if nums[mid] < target <= nums[right]:
+                left = mid + 1
+            else:
+                right = mid - 1
+    return -1
+print(searchInRotatedSortedArray2(nums, target))
+# Time complexity = O(log n)
 # Space complexity = O(1)
