c39adef1540628c29db3f118fa0cfe5782b3ae22
Kareena2070 <kareenayadav1088@gmail.com>
2025-11-25 21:35:09 +0530

    solve 2nd ques find index of 1st character that does not repeat

--- DSA-fromHostel/25-11-25.py
+++ DSA-fromHostel/25-11-25.py
@@ -10,3 +10,17 @@ print("Largest element in the array is:", arr[-1])
 # Space complexity = O(1) as no extra space is used.
 
 
+
+# Given a string s, return the index of the first character that does not repeat.If no such character exists, return -1.
+s = "lleetcode"
+
+for i in range(len(s)):
+    count = 0
+    for j in range(len(s)):
+        if s[i] == s[j]:
+            count += 1
+    if count == 1:
+        print("Index of first non-repeating character is:", i)
+        break
+# Time complexity = O(n^2) where n is the length of the string.
+# Space complexity = O(1) as no extra space is used.
