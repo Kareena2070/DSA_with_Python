546f255b378e9113f847bcb78add2133aa0cc4ab
Kareena2070 <kareenayadav1088@gmail.com>
2025-11-27 20:52:50 +0530

    solve 2nd ques count occurrences of a character

--- DSA-fromHostel/27-11-25.py
+++ DSA-fromHostel/27-11-25.py
@@ -1,4 +1,4 @@
-# 3. Count even numbers in an array
+# 1. Count even numbers in an array
 # Input: [2, 5, 6, 7, 8]
 # Output: 3
 
@@ -11,4 +11,21 @@ for i in range(len(arr)):
 print("Count of even numbers in an array: ", count)
 # Time complexity = O(n) where n is the length of the array.
 # Space complexity = O(1) as no extra space is used.
+
+
+
+
+# 2. Count occurrences of a character
+# Input: string = "banana", char = "a"
+# Output: 3
+
+string = "bananaaa"
+char = "a"
+count = 0
+for i in range(len(string)):
+    if string[i] == char:
+        count+=1
+print("Char count occurrences in a string: ", count)
+
+
 
