175f5e31abfd9cd7a07f7d41a40efb1f8b688a5f
Kareena Yadav <kareenayadav1088@gmail.com>
2025-12-03 21:39:06 +0530

    optimal way to solve running sum

--- leetCodeQuestion/esayLevel/runningSumof1dArray.py
+++ leetCodeQuestion/esayLevel/runningSumof1dArray.py
@@ -34,4 +34,13 @@ print(runningSum(nums))
 # Space complexity = O(n)
 
 
+# in-place modification
+nums = [1,2,3,4]
+def runningSum(nums):
+    for i in range(1, len(nums)):
+        nums[i] += nums[i-1]
+    return nums
+print(runningSum(nums))
+# Time complexity = O(n)
+# Space complexity = O(1)
 
