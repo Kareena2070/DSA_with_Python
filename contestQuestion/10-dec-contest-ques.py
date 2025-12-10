49c77f0aeab911f3bdeb289715e9ff80ae1e38cb
Kareena2070 <kareenayadav1088@gmail.com>
2025-12-10 21:13:01 +0530

    solve 2nd question from codeChef contest

--- contestQuestion/10-dec-contest-ques.py
+++ contestQuestion/10-dec-contest-ques.py
@@ -19,3 +19,26 @@ if k <1000:
 else:
     print(1000)
 
+#Ques 2.  Scoring
+# Alice and Bob were playing a game with points to score. Each player's score is some non-negative integer number of points.
+
+# In the end, Alice won the game by scoring X more points than Bob. Further, it is known that the total number of points scored by both of them was Y.
+
+# With this information, your task is to find how many points Alice scored, and how many points Bob scored. It is guaranteed that the input is generated such that there exists a valid solution.
+
+# Input Format
+# The first line of input will contain a single integer T, denoting the number of test cases.
+# The first and only line of each test case contains 2 integers - X and Y.
+# Output Format
+# For each test case, output on a new line Alice and Bob's scored points.
+
+t = int(input("number of test cases: "))
+for _ in range(t):
+    x, y = list(map(int, input("Alice extra points and total points: ").split()))
+    
+    k = y-x
+    l = k//2
+    r = x+ l 
+    m = y-r
+    print("Alice points:", r, "Bob points:", m)
+    
