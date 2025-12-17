61a4d420fe70a73db29f3dcb9150784f12724d17
Kareena2070 <kareenayadav1088@gmail.com>
2025-12-17 21:02:19 +0530

    solve contest ques-2

--- contestQuestion/17-dec-contest-ques.py
+++ contestQuestion/17-dec-contest-ques.py
@@ -20,5 +20,25 @@ else:
     print(k)
 
 
+# Add to make Positive
+# An array A of length N is called good if the sum of all it's elements is non-negative, i.e. (A1+A2+…+AN)≥0(A 1​+A 2​+…+A N​)≥0.
+
+# Find the minimum non-negative integer X such that if you add X to each element of the array, the array becomes good. That is, an array B of length N, defined as Bi=Ai+XB i​=A i +X, is good.
+
+import math
+t = int(input("Number of test cases: "))
+
+for _ in range(t):
+    n = int(input("Array length: "))
+    a = list(map(int, input("array list: ").split()))
+    
+    x = sum(a)
+    
+    if x >= 0:
+        print(0)
+        
+    else:
+        y = math.ceil((-x)/n)
+        print(y)
 
 
