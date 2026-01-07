1040189c48677a6533f926a5585bb7f76db73cd7
Kareena2070 <kareenayadav1088@gmail.com>
2026-01-08 00:20:49 +0530

    solve 2nd question of codeChef webnesday contest

--- contestQuestion/8-Jan-contest-ques.py
+++ contestQuestion/8-Jan-contest-ques.py
@@ -10,3 +10,22 @@ x = int(input('Number of push-ups per day: '))
 print(x*31)
 
 
+
+# ques 2.
+# Front-Back Matching
+# You are given a string S of length N.The string contains only lowercase English letters.
+
+# You can freely rearrange the characters of S however you like.
+
+# After rearrangement, is it possible to make the first and last characters of S equal?
+
+t = int(input('Number of test cases: '))
+
+for _ in range(t):
+    n = int(input('Length of string: '))
+    s = input('String: ')
+    r = len(set(s))
+    if r < len(s):
+        print('Yes')
+    else:
+        print('No')
