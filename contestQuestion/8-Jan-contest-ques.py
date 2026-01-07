fbd7424eb1e5506588386914bcca85579bf60c16
Kareena2070 <kareenayadav1088@gmail.com>
2026-01-08 00:20:07 +0530

    solve 1st question of codeChef webnesday contest

---
+++ contestQuestion/8-Jan-contest-ques.py
@@ -0,0 +1,12 @@
+#ques 1.
+# New Year Resolution
+# To start the year 2026 off, Chef made a resolution to exercise daily. He decided to do exactly X push-ups every day.
+
+# If he sticks to his resolution, how many push-ups will he do in the month of January?
+# Note that the month of January has 31 days.
+
+x = int(input('Number of push-ups per day: '))
+
+print(x*31)
+
+
