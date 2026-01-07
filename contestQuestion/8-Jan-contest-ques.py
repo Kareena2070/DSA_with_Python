780783f0f9d16b4f01cca87d7a994cc99d33e986
Kareena2070 <kareenayadav1088@gmail.com>
2026-01-08 00:21:26 +0530

    solve 3rd question of codeChef webnesday contest

--- contestQuestion/8-Jan-contest-ques.py
+++ contestQuestion/8-Jan-contest-ques.py
@@ -29,3 +29,39 @@ for _ in range(t):
         print('Yes')
     else:
         print('No')
+
+
+# ques 3.
+
+# Movie Hype
+# Chef decided to watch a movie, and has just entered the theater.
+
+# The theater has 2N+12N+1 seats arranged in a row. The seats are numbered 1 to 2N+12N+1 from left to right.
+
+# Currently, the seats numbered 
+# 1,3,5,7,…,2N+1,3,5,7,…,2N+1 are occupied, and the seats numbered 2,4,6,…,2N2,4,6,…,2N are empty.
+# Chef must decide which empty seat to sit in.
+
+# For each i=1,2,3,…,N+1 i=1,2,3,…,N+1, The person sitting in seat 2i−1 2i−1 has a hype level of A i.That is, the person in seat 1 has a hype level of A 1, the person in seat 3 has a hype level of A 2 , and so on.
+
+# Chef thinks the loudness of an empty seat is the maximum of the hype levels of the two people sitting next to him.
+# So, the loudness of seat 2 is the maximum of the hype levels of the people sitting in seats 1 and 3; the loudness of seat 4 is the maximum of the hype levels of the people sitting in seats 3 and 5; and so on.
+
+# To properly enjoy the movie, Chef would like to choose a seat whose loudness is as low as possible.
+
+# Can you help Chef by finding this minimum loudness?
+
+
+t = int(input('Number of test cases: '))
+
+for _ in range(t):
+    n = int(input('Number of people: '))
+    a = list(map(int, input('seats: ').split()))
+    
+    min_loud = float('inf')
+    
+    for i in range(n):
+        current_loud = max(a[i], a[i+1])
+        if current_loud < min_loud:
+            min_loud = current_loud
+    print(min_loud)
