8f439ab437f8ca9b3513b926fabef938e105a327
Kareena2070 <kareenayadav1088@gmail.com>
2026-01-14 21:14:55 +0530

    solve codeChef contest ques 2

--- contestQuestion/14-Jan-2026-contest-ques.py
+++ contestQuestion/14-Jan-2026-contest-ques.py
@@ -18,3 +18,38 @@ else:
     print("NO")
 
 
+# Winter is Coming
+# Chef feels comfortable depending on the temperature and whether or not he is wearing a jacket.
+
+# Chef feels comfortable without a jacket if and only if the temperature is greater than or equal to A.
+# Chef feels comfortable with a jacket if and only if the temperature is less than or equal to B.It is guaranteed that A≤BA≤B.
+# Chef is not wearing a jacket initially.
+# You are given the temperature TiT i​  for the next N days.
+# Chef can choose to wear or remove his jacket any number of times, but each time he puts on the jacket is counted as one action.
+
+# Find the minimum number of times Chef needs to wear a jacket so that he feels comfortable on all N days.
+
+# Input Format
+# The first line contains a single integer T, denoting the number of test cases.
+# For each test case:
+# The first line contains three space-separated integers N, A, and B.The second line contains N space-separated integers T1,T2,…,TNT 1​ ,T 2​ ,…,T N​, where TiT i​  is the temperature on the i-th day.
+# Output Format
+# For each test case, output a single integer — the minimum number of times Chef needs to wear a jacket.
+
+t = int(input("Test case: "))
+
+for _ in range(t):
+    n, a, b = list(map(int, input('n, a, b: ').split()))
+    t = list(map(int, input("Temperature of everyone: ").split()))
+    
+    curr_state = 0
+    jacket = 0
+    
+    for temp in t:
+        if curr_state == 0 and temp<a:
+            curr_state = 1
+            jacket+=1
+        elif curr_state ==1 and temp >b:
+            curr_state =0
+            
+    print("Minimum number of times Chef wears a jacket is", jacket)
